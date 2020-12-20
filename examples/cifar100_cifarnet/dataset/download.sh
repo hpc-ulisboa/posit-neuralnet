1a426b90ec1905009dc0d731fecd52161203a9ae
gonced8 <gonced8@gmail.com>
2020-12-20 17:32:52 +0000

    cifar-100 dataset

--- examples/cifar100_cifarnet/dataset/download.sh
+++ examples/cifar100_cifarnet/dataset/download.sh
@@ -4,3 +4,12 @@
 curl https://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz --output cifar-100-binary.tar.gz
 
 # Uncompress files
+tar -zxvf cifar-100-binary.tar.gz
+
+# Move from folder
+mv cifar-100-binary/* .
+
+# Delete unnecessary
+rm cifar-100-binary.tar.gz
+rm fine_label_names.txt~
+rm -r cifar-100-binary
